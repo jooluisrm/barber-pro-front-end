df0f8adbba1440254cfe599594cd43523b691b9b
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-02-06 10:56:13 -0300

    criação da tela fazer login

--- src/components/header/headerTop.tsx
+++ src/components/header/headerTop.tsx
@@ -2,13 +2,14 @@ import { CircleUserRound } from 'lucide-react';
 import { LiMenu } from '../liMenu';
 import { ButtonLogin } from '../buttonLogin';
 import { ToggleTheme } from '../toggleTheme';
+import { DialogRegister } from '../dialogRegister';
 
 export const HeaderTop = () => {
     return (
         <div className="container mx-auto flex justify-between items-center">
             <div className='flex gap-10'>
                 <div className='font-extrabold'>LOGO</div>
-                <ul className='flex gap-5'>
+                <ul className='gap-5 hidden md:flex'>
                     <LiMenu text='Início' rota='/'/>
                     <LiMenu text='Buscar' rota='buscar'/>
                     <LiMenu text='Meus Agendamentos' rota='agendamentos'/>
@@ -16,7 +17,7 @@ export const HeaderTop = () => {
             </div>
             <div className='flex items-center gap-5'>
                 <ToggleTheme />
-                <ButtonLogin />
+                <DialogRegister />
             </div>
         </div>
     )
