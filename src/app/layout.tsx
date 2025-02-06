437538f538c6d92834872f42e45bba0ced309068
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-02-06 09:15:28 -0300

    inicio da criação do header pc version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { HeaderTop } from "@/components/header/headerTop";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <header>
+        <HeaderTop />
+      </header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ src/components/header/headerTop.tsx
@@ -0,0 +1,17 @@
+export const HeaderTop = () => {
+    return (
+        <div className="text-white">
+            <div>
+                <div>LOGO</div>
+                <ul>
+                    <li>Início</li>
+                    <li>Buscar</li>
+                    <li>Meus Agendamentos</li>
+                </ul>
+            </div>
+            <div>
+                <button><p>Entrar</p></button>
+            </div>
+        </div>
+    )
+}
