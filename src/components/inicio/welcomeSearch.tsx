aed229855efcfb65a4d77444e33ff626e176ba89
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-02-07 08:10:15 -0300

    section welcomeSearch feita

--- src/components/inicio/welcomeSearch.tsx
+++ src/components/inicio/welcomeSearch.tsx
@@ -1,11 +1,17 @@
 import { pegarDataAtual } from "@/utils/pegarDataAtual";
+import { Input } from "../ui/input";
+import { Search } from "lucide-react";
 
 export const WelcomeSearch = () => {
     return (
-        <section className="container mx-auto">
+        <section className="container mx-auto flex flex-col gap-5 py-10">
             <div>
                 <h1 className="text-2xl">Seja bem vindo(a)</h1>
-                <p className="text-sm">{pegarDataAtual().week}, {pegarDataAtual().day} {pegarDataAtual().month} {pegarDataAtual().year}</p>
+                <p className="text-sm text-gray-400">{pegarDataAtual().week}, {pegarDataAtual().day} {pegarDataAtual().month} {pegarDataAtual().year}</p>
+            </div>
+            <div className="flex items-center ">
+                <Search className="absolute ml-3" size={20}/>
+                <Input className="pl-10 h-14" placeholder="Encontre um estabelecimento"/>
             </div>
         </section>
     );
