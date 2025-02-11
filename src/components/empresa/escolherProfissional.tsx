47fbb205a0d5b0000eb52a6dbd73a48195d02dfc
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-02-11 17:03:19 -0300

    section para mostra horarios diponiveis qnd selecionar o barbeiro

--- src/components/empresa/escolherProfissional.tsx
+++ src/components/empresa/escolherProfissional.tsx
@@ -1,6 +1,7 @@
 import { DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog";
 import { ItemSelecionarProfissional } from "./itemSelecionarProfissional";
 import { AlertEscolhaProfissional } from "./alertEscolhaProfissional";
+import { ItemHorario } from "./itemHorario";
 
 type Props = {
     date: Date | undefined;
@@ -17,11 +18,21 @@ export const EscolherProfissional = ({ date, setDate }: Props) => {
                 </div>
                 <DialogDescription>Selecione um profissional para continuar!</DialogDescription>
             </DialogHeader>
-            <div className="flex gap-5 py-5 flex-wrap">
+            <div className="flex gap-5 py-5 mb-5 flex-wrap border-b-4">
                 <ItemSelecionarProfissional />
             </div>
             <div className="pb-5">
                 <AlertEscolhaProfissional />
+                <div className="py-5">
+                    <DialogTitle>Horários disponíveis</DialogTitle>
+                    <div className="grid grid-cols-4 gap-3 py-5">
+                        <ItemHorario />
+                        <ItemHorario />
+                        <ItemHorario />
+                        <ItemHorario />
+                        <ItemHorario />
+                    </div>
+                </div>
             </div>
         </>
     );
---
+++ src/components/empresa/itemHorario.tsx
@@ -0,0 +1,7 @@
+export const ItemHorario = () => {
+    return (
+        <div className="border border-gray-500 py-2 px-5 font-bold text-center rounded-lg transition-all cursor-pointer hover:border-blue-500">
+            17:30
+        </div>
+    );
+}
