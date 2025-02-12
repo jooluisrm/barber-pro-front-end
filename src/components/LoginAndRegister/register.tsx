e0ca4195428343b690499cf9885c56e872ab58b8
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-02-12 10:51:38 -0300

    Site esta registrando usuarios

--- src/components/LoginAndRegister/register.tsx
+++ src/components/LoginAndRegister/register.tsx
@@ -7,13 +7,14 @@ import { Input } from "../ui/input";
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterData, registerUser } from "@/api/auth/authService";
 
 type Props = {
     backPage: VoidFunction;
 }
 
 const RegisterFormSchema = z.object({
-    nomeCompleto: z
+    nome: z
         .string()
         .min(4, { message: "Nome completo deve ter pelo menos 4 caracteres" }),
     
@@ -29,7 +30,7 @@ const RegisterFormSchema = z.object({
         .regex(/[0-9]/, { message: "Senha deve conter pelo menos um número" }),
         
     
-    celular: z
+        telefone: z
         .string()
         .min(11, { message: "Número de celular inválido" })
         .regex(/^\(\d{2}\) \d{5}-\d{4}$/, { message: "Formato de celular inválido, use (XX) XXXXX-XXXX" }) // Máscara para celular
@@ -43,13 +44,9 @@ export const Register = ({ backPage }: Props) => {
         resolver: zodResolver(RegisterFormSchema)
     });
 
-    const [nomeCompleto, setNomeCompleto] = useState("");
-    const [celular, setCelular] = useState();
-    const [email, setEmail] = useState();
-    const [senha, setSenha] = useState();
-
-    const handleRegisterForm = () => {
-        alert('Enviado')
+    const handleRegisterForm = (data: any) => {
+        const {nome, email, senha, telefone} = data;
+        registerUser({nome, email, senha, telefone});
     }
 
     return (
@@ -68,23 +65,23 @@ export const Register = ({ backPage }: Props) => {
                 <div>
                     <label htmlFor="nome">Nome completo</label>
                     <Input
-                        {...register('nomeCompleto')}
+                        {...register('nome')}
                         type="text"
                         id="nome"
                         placeholder="Informe Seu Nome"
                         autoFocus
 
                     />
-                    {errors.nomeCompleto && <p className="text-sm text-red-600 mt-1">* {errors.nomeCompleto.message as string}</p>}
+                    {errors.nome && <p className="text-sm text-red-600 mt-1">* {errors.nome.message as string}</p>}
                 </div>
                 <div>
                     <label htmlFor="tel">Celular</label>
                     <Input
-                        {...register('celular')}
+                        {...register('telefone')}
                         id="tel"
                         placeholder="Digite Seu Numero"
                     />
-                    {errors.celular && <p className="text-sm text-red-600 mt-1">* {errors.celular.message as string}</p>}
+                    {errors.telefone && <p className="text-sm text-red-600 mt-1">* {errors.telefone.message as string}</p>}
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
